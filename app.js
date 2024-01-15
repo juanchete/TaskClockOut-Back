aa44aa0c790ba7b54e04e8b0e01aa6744ce67a65
juanchete <juanlopezlmg@gmail.com>
2024-01-15 00:55:00 -0400

    Add dependencies and update routes

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // app.js
+require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 const endpoint1Router = require('./src/routes/stocksEndpoint');
-// const endpoint2Router = require('./src/routes/endpoint2');
 
-app.use('/api/endpoint1', endpoint1Router);
-// app.use('/endpoint2', endpoint2Router);
-// // Endpoint 2
-// app.get('/endpoint2', (req, res) => {
-//   res.json({ mensaje: 'Hola desde el Endpoint 2' });
-// });
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+app.use('/api/stocks', endpoint1Router);
 
-// Iniciar el servidor
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- src/routes/stocksEndpoint.js
+++ src/routes/stocksEndpoint.js
@@ -9,21 +9,24 @@ router.get('/:nombre/:fecha', async (req, res) => {
       const { nombre, fecha } = req.params;
       const adjusted = req.query.adjusted || 'defaultAdjustedValue';
   
+      if (!nombre || !fecha) {
+        return res.status(400).json({ error: 'Invalid input parameters' });
+      }
+
       // Construir la URL con los parámetros
-      const apiUrl = `https://api.polygon.io/v1/open-close/${nombre}/${fecha}?adjusted=${adjusted}&apiKey=P4supTYLz41tFSMEaJA2pQdNPb8omtp5`;
+      const apiUrl = `https://api.polygon.io/v1/open-close/${nombre}/${fecha}?adjusted=${adjusted}&apiKey=${process.env.API_KEY}`;
   
-      console.log("HOLAA");
       // Hacer una llamada a la API externa
       const response = await axios.get(apiUrl);
   
       // Responder con los datos de la API externa
-      res.json({
-        message: 'Hello from Endpoint 1',
+      res.status(200).json({
+        message: 'Successs',
         params: { nombre, fecha, adjusted },
         externalData: response.data,
       });
     } catch (error) {
-      console.error('Error al hacer la llamada a la API externa', error);
+      console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
@@ -32,6 +35,11 @@ router.get('/watchlist', async (req, res) => {
         const adjusted = req.query.adjusted || 'false';
         const date = req.query.date || 'false';
     
+        // Validate input
+    if (!adjusted || !date) {
+      return res.status(400).json({ error: 'Invalid input parameters' });
+    }
+
         const companies = [
           { shortName: 'SPOT', longName: 'Spotify', imageURL: 'https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs' },
           { shortName: 'AAPL', longName: 'Apple', imageURL: "https://media.designrush.com/inspiration_images/134802/conversions/_1511456315_653_apple-mobile.jpg" },
@@ -41,7 +49,7 @@ router.get('/watchlist', async (req, res) => {
     
         const data = await Promise.all(
           companies.map(async (stock) => {
-            const apiUrl = `https://api.polygon.io/v1/open-close/${stock.shortName}/${date}?adjusted=${adjusted}&apiKey=P4supTYLz41tFSMEaJA2pQdNPb8omtp5`;
+            const apiUrl = `https://api.polygon.io/v1/open-close/${stock.shortName}/${date}?adjusted=${adjusted}&apiKey=${process.env.API_KEY}`;
             const response = await axios.get(apiUrl);
     
             return {
@@ -56,13 +64,13 @@ router.get('/watchlist', async (req, res) => {
         );
     
         // Responder con los datos de la API externa
-        res.json({
-          message: 'Hello from Endpoint 1.1',
+        res.status(200).json({
+          message: 'Watchlist Success',
           params: { date, adjusted },
           data: data,
         });
       } catch (error) {
-        console.error('Error al hacer la llamada a la API externa', error);
+        console.error( error);
         res.status(500).json({ error: 'Internal Server Error' });
       }
   });
